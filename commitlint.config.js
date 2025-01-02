a40ab24d81f0a36972f7b1b356d0c9b0dfdedcd2
s4shibam <shibamsaha2002@gmail.com>
2025-01-03 00:38:53 +0530

    chore: add commit linting configuration

---
+++ commitlint.config.js
@@ -0,0 +1,42 @@
+/**
+ * Commitlint configuration
+ * @see https://commitlint.js.org/#/reference-configuration
+ */
+const config = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // Override rules here if needed
+    'body-max-line-length': [2, 'always', 100],
+    'footer-max-line-length': [2, 'always', 100],
+    'header-max-length': [2, 'always', 100],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
+    ],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test'
+      ]
+    ]
+  }
+}
+
+module.exports = config
