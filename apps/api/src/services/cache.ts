9d16fd0704292c4e76734de587473ffc8673dc79
s4shibam <shibamsaha2002@gmail.com>
2025-05-25 18:47:22 +0530

    refactor: update prefix for rate limiting and caching services

--- apps/api/src/services/cache.ts
+++ apps/api/src/services/cache.ts
@@ -1,9 +1,10 @@
-import { CacheFactory } from '@mizzo/redis'
+import { CacheService } from '@mizzo/redis'
+import { APP_SLUG } from '@mizzo/utils'
 
-export const cache = CacheFactory.create({
+export const cache = new CacheService({
   app: 'API',
-  config: {
-    defaultTtl: 3600,
-    prefix: 'cache'
+  defaultConfig: {
+    ttl: 3600,
+    prefix: `${APP_SLUG}:cache`
   }
 })
