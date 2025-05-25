419c033b46abb9f74d0915731efb98cad43f9b7a
s4shibam <shibamsaha2002@gmail.com>
2025-05-25 16:02:33 +0530

    feat: add logger dependency and better error handling

--- packages/redis/src/index.ts
+++ packages/redis/src/index.ts
@@ -1,10 +1,18 @@
 import Redis from 'ioredis'
 
+import { log } from '@mizzo/logger'
 import { NODE_ENV } from '@mizzo/utils'
 
 const globalForRedis = global as unknown as { redis: Redis }
 
 export const redis = globalForRedis.redis || new Redis(process.env.REDIS_URL!)
+
+redis.on('error', (error) => {
+  log.error({
+    message: 'Redis connection error',
+    meta: { error }
+  })
+})
 
 if (NODE_ENV !== 'prod') {
   globalForRedis.redis = redis
@@ -21,7 +29,8 @@ export const disconnectRedis = async (): Promise<void> => {
   }
 }
 
-export * from './cache'
+export * from './cache/index'
+
 export * from './ratelimit/fixed-window'
 export * from './ratelimit/index'
 export * from './ratelimit/token-bucket'
