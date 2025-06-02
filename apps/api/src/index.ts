6164a06236d7e691b52c0a39d1815d1b90435bc1
s4shibam <shibamsaha2002@gmail.com>
2025-06-03 04:08:26 +0530

    feat: add serverless configuration and new env vars

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,43 +1,12 @@
-import { API_PORT, NODE_ENV } from '@mizzo/utils'
+import serverless from 'serverless-http'
+
+import 'source-map-support/register'
 
 import app from './app'
 
-const startServer = async () => {
-  try {
-    app.listen(API_PORT, () => {
-      if (NODE_ENV === 'dev') {
-        console.log(`Server is live on: http://localhost:${API_PORT}`)
-      } else {
-        console.log(`Server is live on PORT: ${API_PORT}`)
-      }
-    })
-  } catch (error) {
-    console.error('Failed to start server...\n', error)
-    process.exit(1)
-  }
-}
-
-const gracefulShutdown = async () => {
-  try {
-    console.log('Shutting down gracefully...\n')
-    process.exit(0)
-  } catch (error) {
-    console.error('Error during shutdown...\n', error)
-    process.exit(1)
-  }
-}
-
 const handleFatalError = (error: Error, type: 'rejection' | 'exception') => {
-  console.error(`Shutting down due to unhandled ${type}...\n`, error)
-  gracefulShutdown()
+  console.error(`Lambda error - unhandled ${type}...\n`, error)
 }
-
-// Start the server
-startServer()
-
-// Handle graceful shutdown
-process.on('SIGTERM', gracefulShutdown)
-process.on('SIGINT', gracefulShutdown)
 
 process.on('unhandledRejection', (error: Error) => {
   handleFatalError(error, 'rejection')
@@ -46,3 +15,5 @@ process.on('unhandledRejection', (error: Error) => {
 process.on('uncaughtException', (error: Error) => {
   handleFatalError(error, 'exception')
 })
+
+export const handler = serverless(app)
