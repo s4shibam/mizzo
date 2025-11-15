6c819e4210cf2f18eb8013e6ca52a3ab3f04ad91
s4shibam <s4shibam@gmail.com>
2025-11-15 07:38:09 +0530

    feat: enhance track update functionality to send SQS message for reprocessing as needed

--- apps/api/src/controllers/admin/update-track.ts
+++ apps/api/src/controllers/admin/update-track.ts
@@ -2,6 +2,11 @@ import { Request, Response } from 'express'
 
 import { z } from 'zod'
 
+import {
+  env as awsEnv,
+  sqsBuildS3EventBridgeMessage,
+  sqsSendMessage
+} from '@mizzo/aws'
 import { prisma, Status } from '@mizzo/prisma'
 
 import { STATUS_LIST } from '../../constants/common'
@@ -24,7 +29,8 @@ export const updateTrack = async (req: Request, res: Response) => {
     },
     select: {
       title: true,
-      primaryArtistId: true
+      primaryArtistId: true,
+      trackKey: true
     }
   })
 
@@ -44,6 +50,28 @@ export const updateTrack = async (req: Request, res: Response) => {
     trackTitle: track.title,
     status
   })
+
+  // If status is set to PENDING, send message to SQS to reprocess the track
+  if (status === 'PENDING' && track.trackKey) {
+    const s3EventBridgeMessage = sqsBuildS3EventBridgeMessage({
+      bucketName: awsEnv.awsS3Bucket,
+      key: track.trackKey
+    })
+
+    const messageSent = await sqsSendMessage(s3EventBridgeMessage)
+
+    if (messageSent) {
+      res.status(201).json({
+        message: `Track status updated to ${getStatusText({ status })} and queued for reprocessing.`
+      })
+      return
+    } else {
+      res.status(201).json({
+        message: `Track status updated to ${getStatusText({ status })}, but failed to queue for reprocessing.`
+      })
+      return
+    }
+  }
 
   res.status(201).json({
     message: `Track status updated to ${getStatusText({ status })}`
