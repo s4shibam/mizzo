658a52e8c70650547c0cb82caebda6811b34fb2b
s4shibam <s4shibam@gmail.com>
2026-01-10 23:01:37 +0530

    style: update artist and playlist card components

--- apps/web/src/components/cards/playlist.tsx
+++ apps/web/src/components/cards/playlist.tsx
@@ -57,7 +57,7 @@ export const PlaylistCard = ({
       </div>
 
       <div className="px-3 py-2">
-        <p className="line-clamp-1 font-medium lg:text-lg">{playlist?.name}</p>
+        <p className="line-clamp-1 font-medium">{playlist?.name}</p>
         <p className="line-clamp-2 text-sm">{playlistDescription}</p>
       </div>
     </Link>
