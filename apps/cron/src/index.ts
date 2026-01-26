1160295c9168cda33a8098484b6c15f6e0105f96
s4shibam <s4shibam@gmail.com>
2026-01-27 03:02:02 +0530

    refactor: move temporal worker logic to a separate module

--- apps/cron/src/index.ts
+++ apps/cron/src/index.ts
@@ -1,44 +1,5 @@
-import { NativeConnection, Worker } from '@temporalio/worker'
-
-import { env } from './env'
 import { scheduleSqsConsumer } from './jobs'
-import * as activities from './temporal/activities'
-import { MAX_CONCURRENT_AI_ACTIVITIES } from './utils'
-
-const startTemporalWorker = async () => {
-  if (!env.enableTemporal) {
-    console.warn('[CRON] Temporal worker disabled by feature flag')
-    return
-  }
-
-  if (
-    env.temporalApiKey === 'NA' ||
-    env.temporalAddress === 'NA' ||
-    env.temporalNamespace === 'NA'
-  ) {
-    console.warn('[CRON] Temporal configuration missing, skipping worker')
-    return
-  }
-
-  const connection = await NativeConnection.connect({
-    address: env.temporalAddress,
-    tls: true,
-    metadata: {
-      authorization: `Bearer ${env.temporalApiKey}`
-    }
-  })
-
-  const worker = await Worker.create({
-    connection,
-    namespace: env.temporalNamespace,
-    taskQueue: 'track-live-lyric',
-    workflowsPath: require.resolve('./temporal/workflows'),
-    activities,
-    maxConcurrentActivityTaskExecutions: MAX_CONCURRENT_AI_ACTIVITIES
-  })
-
-  await worker.run()
-}
+import { startTemporalWorker } from './temporal/worker'
 
 const startCronServer = () => {
   try {
